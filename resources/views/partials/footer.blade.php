5459794a42ea60a77efe88e697fee0283fbb6c01
usama yasin <yasinusama414@gmail.com>
2026-05-03 10:52:36 +0500

    updated footer and style desings

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,26 +1,26 @@
     <!-- Footer Section -->
-    <footer class="footer inner-footer footer-info">
-        <div class="footer-top">
+    <footer class="footer inner-footer footer-info bg-primary">
+        <div class="footer-top bg-primary" style="background: var(--primary);">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-8">
                         <div class="row">
                             <div class="col-lg-6 col-md-3 col-sm-6">
-                                <div class="footer-widget footer-menu">
-                                    <h6 class="footer-title">Company</h6>
+                                <div class="footer-widget footer-menu text-white">
+                                    <h6 class="footer-title text-white">Company</h6>
                                     <ul>
-                                        <li><a href="{{ url('about-us') }}">About</a></li>
-                                        <li><a href="{{ url('contact-us') }}">Contact</a></li>
-                                        <li><a href="{{ url('doctors') }}">Doctors</a></li>
+                                        <li><a class="text-white" href="{{ url('about-us') }}">About</a></li>
+                                        <li><a class="text-white" href="{{ url('contact-us') }}">Contact</a></li>
+                                        <li><a class="text-white" href="{{ url('doctors') }}">Doctors</a></li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="col-lg-6 col-md-3 col-sm-6">
                                 <div class="footer-widget footer-menu">
-                                    <h6 class="footer-title">Specialities</h6>
+                                    <h6 class="footer-title text-white">Specialities</h6>
                                     <ul>
                                         @foreach(topSpeacilalization() as $specialization)
-                                            <li><a href="{{ url('/doctors?category='.$specialization['name'].'') }}">{{ $specialization['name'] }}</a></li>
+                                            <li><a class="text-white" href="{{ url('/doctors?category='.$specialization['name'].'') }}">{{ $specialization['name'] }}</a></li>
                                         @endforeach
                                     </ul>
                                 </div>
@@ -30,18 +30,18 @@
                     </div>
                     <div class="col-xl-4 col-md-7">
                         <div class="footer-widget">
-                            <h6 class="footer-title">Newsletter</h6>
-                            <p class="mb-2 text-dark">Subscribe & Stay Updated from the PriGina Global Telemed</p>
+                            <h6 class="footer-title text-white">Newsletter</h6>
+                            <p class="mb-2 text-dark text-white">Subscribe & Stay Updated from the PriGina Global Telemed</p>
                             <div class="subscribe-input">
                                 <form action="#">
                                     <input type="email" class="form-control" placeholder="Enter Email Address">
                                     <button type="submit"
                                         class="btn btn-md btn-primary-gradient d-inline-flex align-items-center"><i
-                                            class="isax isax-send-25 me-1"></i>Send</button>
+                                            class="isax isax-send-25 me-1 "></i>Send</button>
                                 </form>
                             </div>
                             <div class="social-icon">
-                                <h6 class="mb-3 footer-title">Connect With Us</h6>
+                                <h6 class="mb-3 footer-title text-white">Connect With Us</h6>
                                 <ul>
                                     <li>
                                         <a href="#"><i class="fa-brands fa-facebook"></i></a>
@@ -70,7 +70,7 @@
                 <!-- Copyright -->
                 <div class="copyright">
                     <div class="copyright-text mb-0">
-                        <p class="mb-0">Copyright © 2025 PriGina Global Telemed. All Rights Reserved</p>
+                        <p class="mb-0 ">Copyright © 2025 PriGina Global Telemed. All Rights Reserved</p>
                     </div>
                     <!-- Copyright Menu -->
                     <div class="copyright-menu">
@@ -80,21 +80,7 @@
                             <li><a href="#">Refund Policy</a></li>
                         </ul>
                     </div>
-                    <!-- /Copyright Menu -->
-                    <ul class="payment-method">
-                        <li><a href="#"><img src="{{ URL::asset('build/img/icons/card-01.svg') }}"
-                                    alt="Img"></a></li>
-                        <li><a href="#"><img src="{{ URL::asset('build/img/icons/card-02.svg') }}"
-                                    alt="Img"></a></li>
-                        <li><a href="#"><img src="{{ URL::asset('build/img/icons/card-03.svg') }}"
-                                    alt="Img"></a></li>
-                        <li><a href="#"><img src="{{ URL::asset('build/img/icons/card-04.svg') }}"
-                                    alt="Img"></a></li>
-                        <li><a href="#"><img src="{{ URL::asset('build/img/icons/card-05.svg') }}"
-                                    alt="Img"></a></li>
-                        <li><a href="#"><img src="{{ URL::asset('build/img/icons/card-06.svg') }}"
-                                    alt="Img"></a></li>
-                    </ul>
+      
                 </div>
                 <!-- /Copyright -->
             </div>
